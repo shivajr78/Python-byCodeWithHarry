1a4273c2a49fe024e5177955efc1d9bd08cf2f10
G Shiva <96604363+shivajr78@users.noreply.github.com>
2023-10-12 10:45:53 +0530

    Add files via upload

---
+++ 3. Strings/02_string_slicing.py
@@ -0,0 +1,20 @@
+# greeting = "Good Morning, "
+# name = "Harry"
+# print(type(name))
+
+# Concatenating two strings
+# c = greeting + name
+# print(c)
+#name = "Harry"
+# print(name[4])
+# name[3] = "d" --> Does not work
+
+# print(name[1:4])
+# print(name[:4]) # is same as name[0:4]
+# print(name[1:]) # is same as name[1:5]
+# c = name[-4:-1] # is same is name[1:4]
+# print(c)
+
+x = "Bamboozled"
+#d = name[:0:-1]
+print(id('shiva'))
